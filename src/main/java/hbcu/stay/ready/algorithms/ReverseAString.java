f9d904668c673417e3e9e4d83baadae3d40cd918
adolison <amdol1@morgan.edu>
2019-07-31 16:44:39 -0400

    Finished

--- src/main/java/hbcu/stay/ready/algorithms/ReverseAString.java
+++ src/main/java/hbcu/stay/ready/algorithms/ReverseAString.java
@@ -1,9 +1,31 @@
 package hbcu.stay.ready.algorithms;
 
+import java.util.Locale;
+
 public class ReverseAString {
 
     public String reverseAString(String input){
-        return null;
+        input = input.toLowerCase();
+        //System.out.println(input.);
+
+        for(int i=0;i<input.length();i++){
+            input= input.substring(0,input.length()-i)+input.substring(0,1)+input.substring(input.length()-i);
+            input = input.substring(1);
+        }
+
+
+       for (int j=0;j<input.length();j++){
+           input = input.substring(0,1).toUpperCase()+input.substring(1);
+            if (input.substring(j,j+1).contains(" ")){
+                //System.out.println("yes");
+                //System.out.println(input.substring(j+1,j+2).toUpperCase());
+                input = input.substring(0,j+1)+input.substring(j+1,j+2).toUpperCase()+input.substring(j+2);
+            }
+        }
+
+
+
+        return input;
     }
 
 
